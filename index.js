41c526cac287a91ee3b289102345d7ce81540b93
Tobias Koppers <tobias.koppers@googlemail.com>
2013-01-30 18:01:33 +0100

    fix for webpack 0.9

--- index.js
+++ index.js
@@ -4,22 +4,23 @@
 */
 var path = require("path");
 var loaderUtils = require("loader-utils");
-module.exports = function() {
+module.exports = function() {};
+module.exports.pitch = function(req) {
 	this.cacheable && this.cacheable();
-	this.clearDependencies && this.clearDependencies()
 	var query = loaderUtils.parseQuery(this.query);
-	var req = loaderUtils.getRemainingRequest(this);
 	var source = [];
-	if(this.web) {
-		source.push("require(" + JSON.stringify(path.join(__dirname, "web.js")) + ");");
+	if(this.target == "web") {
+		source.push("require(" + JSON.stringify("!!" + path.join(__dirname, "web.js")) + ");");
 		source.push("mocha.setup(" + JSON.stringify(query["interface"] || "bdd") + ");");
-		source.push("require(" + JSON.stringify(req) + ")");
+		source.push("require(" + JSON.stringify("!!" + req) + ")");
 		source.push("mocha.run();");
-	} else {
-		source.push('var EnhancedMocha = require(' + JSON.stringify(path.join(__dirname, "EnhancedMocha.js")) + ');');
+	} else if(this.target == "node") {
+		source.push('var EnhancedMocha = require(' + JSON.stringify("!!" + path.join(__dirname, "EnhancedMocha.js")) + ');');
 		source.push('var mocha = new EnhancedMocha({reporter: ' + JSON.stringify(query.reporter || "spec") + '});');
-		source.push('mocha.addFile(' + JSON.stringify(req) + ');');
+		source.push('mocha.addFile(' + JSON.stringify("!!" + req) + ');');
 		source.push('mocha.watch();');
+	} else {
+		throw new Error("Unsupported target environment " + this.target);
 	}
 	return source.join("\n");
 }
