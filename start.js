4f52558c2cbaf29482b92f054c05d130d0aca843
Casey Webb <notcaseywebb@gmail.com>
2015-03-08 16:38:14 -0500

    Add saucelabs compatibility

--- start.js
+++ start.js
@@ -2,6 +2,8 @@ process.nextTick(function() {
 	delete require.cache[module.id];
 	if(typeof window !== "undefined" && window.mochaPhantomJS)
 		mochaPhantomJS.run();
+	if(typeof window !== "undefined" && window.mochaSaucePlease)
+		mochaSaucePlease();
 	else
 		mocha.run();
 });
