8f4bb85c2d61cdc154ac7685e6eaff9241c41d30
shellscape <andrew@shellscape.org>
2018-04-07 23:18:54 -0400

    refactor(defaults): update to latest webpack-defaults

--- example/test.js
+++ example/test.js
@@ -1,14 +1,19 @@
-describe("test-case", function() {
-	it("should run", function(done) {
-		setTimeout(done, 1000);
-	});
-	it("should fail", function(done) {
-		setTimeout(function() {
-			throw new Error("Fail");
-		}, 1000);
-	});
-	it("should randomly fail", function() {
-		if(require("./dep"))
-			throw new Error("Random fail");
-	});
+/* eslint prefer-arrow-callback: off, global-require: off */
+
+describe('test-case', function d() {
+  it('should run', function it(done) {
+    setTimeout(done, 1000);
+  });
+
+  it('should fail', function i() {
+    setTimeout(function timeout() {
+      throw new Error('Fail');
+    }, 1000);
+  });
+
+  it('should randomly fail', function i() {
+    if (require('./dep')) {
+      throw new Error('Random fail');
+    }
+  });
 });
