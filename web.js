e9bf2386e2e0512696e37a06815d06c5d6ff25db
Andrew Powell <shellscape@users.noreply.github.com>
2018-04-30 08:09:20 -0400

    refactor(defaults): update to latest webpack-defaults (#73)
    BREAKING CHANGE: Enforces `engines` of `"node": ">=6.9.0 < 7.0.0 || >= 8.9.0`

--- web.js
+++
@@ -1,4 +0,0 @@
-if (! document.getElementById("mocha")) { document.write("<div id=\"mocha\"></div>"); }
-
-require("!style-loader!css-loader!mocha/mocha.css");
-require("!script-loader!mocha/mocha.js");
